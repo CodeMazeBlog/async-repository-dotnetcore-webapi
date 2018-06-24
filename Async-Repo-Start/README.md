b85d5972dede35a289c13918f318e61af62f8d4b
Marinko <marinkospasojevic@gmail.com>
2018-06-24 14:17:26 +0200

    Starting project for async repository

---
+++ Async-Repo-Start/README.md
@@ -0,0 +1,3 @@
+## async-repository-dotnetcore-webapi
+# Async Repository in ASP.NET Core Web API - start project
+ https://code-maze.com/async-generic-repository-pattern/
---
+++ Async-Repo-Start/Repository/AccountRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Contracts;
+using Entities;
+using Entities.Models;
+
+namespace Repository
+{
+    public class AccountRepository: RepositoryBase<Account>, IAccountRepository
+    {
+        public AccountRepository(RepositoryContext repositoryContext)
+            :base(repositoryContext)
+        {
+        }
+    }
+}
